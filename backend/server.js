ae21c72fe2e237ccc597e05352e7d6dac61174e5
Erick <ericksilvablue@gmail.com>
2022-02-10 22:10:49 -0300

    Configuração e rotas iniciais do Express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('Hello, World!')
+import express from 'express'
+import colors from 'colors'
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (_, res) => res.status(200).send('Running!'))
+
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        message: 'Running!',
+        data: {
+            query: req.query,
+            params: req.params,
+            body: req.body,
+        },
+    })
+})
+
+
+const server = app.listen(port, () => console.log(colors.blue.bold(`Goalsetter running on port ${port}`)))
+
+// console.log(server)
+// console.log(process.env, process.env.PORT)
+
+process.addListener('uncaughtException', e => console.log(e))
+process.addListener('unhandledRejection', e => console.log(e))
